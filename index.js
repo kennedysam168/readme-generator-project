b39a99af285980d35aac0469ea1b42e1779ac45b
Sam Kennedy <kennedysam168@gmail.com>
2021-11-01 17:08:33 -0400

    added inquirer prompts

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 const inquirer = require('inquirer');
+const fs = require("fs");
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'Project title',
+    },
+    {
+      type: 'input',
+      message: 'Describe your project.',
+      name: 'Description',
+    },
+    {
+      type: 'input',
+      message: 'Enter a table of content.',
+      name: 'Table of Tontent',
+    },
+    {
+      type: 'input',
+      message: 'Describe how to install your project.',
+      name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'Describe how to use your project.',
+        name: 'Usage',
+    },
+    {
+        type: 'list',
+        message: '',
+        name: 'Lisence',
+        choices: []
+    },
+    {
+        type: 'input',
+        message: '',
+        name: 'Contributing',
+    },
+    {
+        type: 'input',
+        message: '',
+        name: 'Tests',
+    },
+    {
+        type: 'input',
+        message: 'Enter your Github Username.',
+        name: 'Username',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email.',
+        name: 'Email',
+    },
+  ])
